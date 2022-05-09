e9bb40b26396b249dec4e1100e5cea9bd298d23f
ckpd3453 <98898121+ckpd3453@users.noreply.github.com>
2022-05-10 00:18:57 +0530

    Created Employee Payroll Spring Project ro Print Welcome to Employee Payroll Services

---
+++ src/main/java/com/bridgelabz/EmployeepayRoll/EmployeepayRollApplication.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.EmployeepayRoll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeepayRollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeepayRollApplication.class, args);
+
+		System.out.println("***********Welcome to Employee PayRoll Services************");
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/EmployeepayRoll/EmployeepayRollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.EmployeepayRoll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeepayRollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
