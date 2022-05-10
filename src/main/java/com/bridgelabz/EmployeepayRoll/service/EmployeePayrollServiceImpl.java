f7407333627d71b45a5075ca763f70a9731b1b7d
ckpd3453 <98898121+ckpd3453@users.noreply.github.com>
2022-05-10 11:01:21 +0530

    Implemented Lombok Library for using getter setter and logging

--- src/main/java/com/bridgelabz/EmployeepayRoll/service/EmployeePayrollServiceImpl.java
+++ src/main/java/com/bridgelabz/EmployeepayRoll/service/EmployeePayrollServiceImpl.java
@@ -2,29 +2,23 @@ package com.bridgelabz.EmployeepayRoll.service;
 
 import com.bridgelabz.EmployeepayRoll.dto.EmployeePayrollDTO;
 import com.bridgelabz.EmployeepayRoll.model.EmployeePayrollData;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Service
 public class EmployeePayrollServiceImpl implements IEmployeePayrollService {
 
     private List<EmployeePayrollData> employeePayrollDataList = new ArrayList<>();
 
-    @Override
-    public List<EmployeePayrollData> getEmployeePayrollData() {
-        return employeePayrollDataList;
-    }
-
-    @Override
-    public EmployeePayrollData getEmployeePayrollDataById(int empId) {
-        return employeePayrollDataList.get(empId-1);//as arraylist index is starts from 0
-    }
 
     @Override
     public EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO employeePayrollDTO) {
+        log.info("Adding Employee Data to ArrayList");
         EmployeePayrollData employeePayrollData = null;
         employeePayrollData = new EmployeePayrollData(employeePayrollDataList.size()+1, employeePayrollDTO);
         employeePayrollDataList.add(employeePayrollData);//adds in employee list
@@ -32,7 +26,20 @@ public class EmployeePayrollServiceImpl implements IEmployeePayrollService {
     }
 
     @Override
+    public List<EmployeePayrollData> getEmployeePayrollData() {
+        log.info("Getting the list of Employee Payroll Data");
+        return employeePayrollDataList;
+    }
+
+    @Override
+    public EmployeePayrollData getEmployeePayrollDataById(int empId) {
+        log.info("Getting Employee Payroll data by ID");
+        return employeePayrollDataList.get(empId-1);//as arraylist index is starts from 0
+    }
+
+    @Override
     public EmployeePayrollData updateEmployeePayrollData(int empId, EmployeePayrollDTO employeePayrollDTO) {
+        log.info("Editing/Update Employee Payroll data");
         EmployeePayrollData employeePayrollData = this.getEmployeePayrollDataById(empId);
         employeePayrollData.setName(employeePayrollDTO.name);
         employeePayrollData.setSalary(employeePayrollDTO.salary);
@@ -41,7 +48,9 @@ public class EmployeePayrollServiceImpl implements IEmployeePayrollService {
     }
 
     @Override
-    public void deleteEmployeePayrollData(int empId) {
+    public void deleteEmployeePayrollData(int empId)
+    {
+        log.info("Deleting the Employee Payroll Data of given Employee ID");
         employeePayrollDataList.remove(empId-1);
     }
 }
